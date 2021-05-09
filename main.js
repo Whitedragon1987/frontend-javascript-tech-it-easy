6e96d2eada9f9b4b4a0e0eb7c2a38a876483762e
Whitedragon1987 <vanoostenjohan@hotmail.com>
2021-05-09 18:22:33 +0200

    'alldone'

--- main.js
+++ main.js
@@ -161,3 +161,197 @@ const inventory = [
     sold: 8,
   },
 ];
+
+function getTotalTvsSold() {
+  const remainingTv = inventory.map(function (stock) {
+    return stock.originalStock - stock.sold;
+  })
+  let totalTvsSold = 0
+
+  for (let i = 0; i < remainingTv.length; i++) {
+    totalTvsSold += remainingTv[i];
+  }
+  return totalTvsSold;
+}
+
+const numbersOfTvs = document.getElementById("tvsInStock");
+numbersOfTvs.textContent = "Nog te verkopen tv's: " + getTotalTvsSold();
+document.body.appendChild(numbersOfTvs);
+
+const tvTypes = inventory.map((inventory) => {
+  return (inventory.type);
+});
+
+const tvsSoldOut = inventory.filter((inventory) => {
+  return (inventory.originalStock === inventory.sold);
+} );
+
+const tvAmbilight = inventory.filter((inventory) => {
+  return (inventory.options.ambiLight === true);
+});
+
+const tvsSortedOnPrice = inventory.sort((a,b) => {
+  if (a.price > b.price) {
+    return 1;
+  }
+  if (a.price < b.price) {
+    return -1;
+  }
+  else return 0;
+});
+
+function getGoalAmount() {
+  const targetPerType = inventory.map((inventory) => {
+    return (inventory.originalStock * inventory.price);
+  });
+
+    let totalTarget = 0;
+
+    for (let i = 0; i < targetPerType.length; i++) {
+      totalTarget += targetPerType[i];
+    }
+    return totalTarget;
+}
+
+const amountOfTarget = document.getElementById("targetAmount");
+amountOfTarget.textContent = "Te behalen omzet: " + getGoalAmount();
+document.body.appendChild(amountOfTarget);
+
+function getAmountSold() {
+  const tvsSoldSoFar = inventory.map((inventory) => {
+    return inventory.sold * inventory.price;
+  })
+
+    let totalSold = 0;
+
+    for (let i = 0; i < tvsSoldSoFar.length; i++) {
+      totalSold += tvsSoldSoFar[i];
+    }
+    return totalSold;
+}
+
+const amountSoFarSold = document.getElementById("soldSoFar");
+amountSoFarSold.textContent = "Omzet tot nu toe: " + getAmountSold();
+document.body.appendChild(amountSoFarSold);
+
+const tvType1 = inventory[1].type;
+
+const tvTypeToShowFirst = document.getElementById("tvType1");
+tvTypeToShowFirst.textContent = "Tv Type: " + tvType1;
+document.body.appendChild(tvTypeToShowFirst);
+
+function tvModelTypeName(inventory){
+  return inventory.brand + " " + inventory.type + " - " + inventory.name;
+};
+
+function tvPrice(inventory){
+    return  "\u20AC " + inventory.price + ",-"
+};
+
+function getAllSizes(inventory){
+  return inventory.availableSizes.map((tvSize)=> {
+ return tvSize + " inch (" + (Math.round(tvSize * 2.54)) + " centimeters) "
+})};
+
+const tvType2Name =(tvModelTypeName({
+  type: '50PUS7304/12',
+  name: 'The One 4K TV',
+  brand: 'Philips',
+  price: 479,
+  availableSizes: [43, 50, 55, 58, 65, 70],
+  refreshRate: 50,
+  screenType: 'LED',
+  screenQuality: 'Ultra HD/4K',
+  smartTv: true,
+  options: {
+    wifi: true,
+    speech: true,
+    hdr: true,
+    bluetooth: true,
+    ambiLight: true,
+  },
+  originalStock: 8,
+  sold: 3,
+}))
+const tvType2Price = tvPrice({
+  type: '50PUS7304/12',
+  name: 'The One 4K TV',
+  brand: 'Philips',
+  price: 479,
+  availableSizes: [43, 50, 55, 58, 65, 70],
+  refreshRate: 50,
+  screenType: 'LED',
+  screenQuality: 'Ultra HD/4K',
+  smartTv: true,
+  options: {
+    wifi: true,
+    speech: true,
+    hdr: true,
+    bluetooth: true,
+    ambiLight: true,
+  },
+  originalStock: 8,
+  sold: 3,
+})
+const tvType2AvailableSizes = getAllSizes({
+  type: '50PUS7304/12',
+  name: 'The One 4K TV',
+  brand: 'Philips',
+  price: 479,
+  availableSizes: [43, 50, 55, 58, 65, 70],
+  refreshRate: 50,
+  screenType: 'LED',
+  screenQuality: 'Ultra HD/4K',
+  smartTv: true,
+  options: {
+    wifi: true,
+    speech: true,
+    hdr: true,
+    bluetooth: true,
+    ambiLight: true,
+  },
+  originalStock: 8,
+  sold: 3,
+}).join("|")
+
+
+const tvTypeToShowSecondName = document.getElementById("tvType2Name");
+tvTypeToShowSecondName.textContent = (tvType2Name);
+document.body.appendChild(tvTypeToShowSecondName);
+
+const tvTypeToShowSecondPrice = document.getElementById("tvType2Price");
+tvTypeToShowSecondPrice.textContent = (tvType2Price);
+document.body.appendChild(tvTypeToShowSecondPrice);
+
+const tvTypeToShowSecondSizes = document.getElementById("tvType2Sizes");
+tvTypeToShowSecondSizes.textContent = (tvType2AvailableSizes);
+document.body.appendChild(tvTypeToShowSecondSizes);
+
+
+
+function tvModelTypeName(inventory){
+  return inventory.brand + " " + inventory.type + " - " + inventory.name;
+};
+
+function tvPrice(inventory){
+  return  "\u20AC " + inventory.price + ",-"
+};
+//
+function getAllSizes(inventory){
+  return inventory.availableSizes.map((tvSize)=> {
+    return tvSize + " inch (" + (Math.round(tvSize * 2.54)) + " centimeters) "
+  })};
+
+function probeersel(inventory){
+  return inventory.map((tv) => {
+    let nameAndPrice = (tv.brand + " " + tv.type + " - " + tv.name + " ")
+    let price = "\u20AC " + tv.price + ",-" + " "
+    let availableSizes = tv.availableSizes.map((tvSize) => {
+      return tvSize + " inch (" + (Math.round(tvSize * 2.54)) + " centimeters) "
+    })
+    return nameAndPrice  + price + availableSizes
+  })}
+
+const allTvsInInventory = document.getElementById("allTvsInInventory");
+allTvsInInventory.textContent = (probeersel(inventory));
+document.body.appendChild(allTvsInInventory);
